462c14355a7910a3cddce1b21e831b71ea70044a
ivan <ivan@haxibiao.com>
2018-08-19 13:43:47 +0800

    刷新所有ios的version number 到 1.0

---
+++ push.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd /data/app/ainicheng
+git push
+
+cd /data/app/dongmeiwei
+git push
+
+cd /data/app/dongdianyi
+git push
+
+cd /data/app/dongdianyao
+git push
+
+cd /data/app/qunyige
+git push
+
+cd /data/app/dianmoge
+git push
+
+cd /data/app/youjianqi
+git push
