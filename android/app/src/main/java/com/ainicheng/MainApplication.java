46dd3c30b87ec9cd45a17ef9268fd5ae5b7c7722
ivan <ivan@haxibiao.com>
2018-08-21 18:42:43 +0800

    升级RN 0.56后，优化了android

--- android/app/src/main/java/com/ainicheng/MainApplication.java
+++ android/app/src/main/java/com/ainicheng/MainApplication.java
@@ -4,7 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
-import com.bugsnag.BugsnagReactNative;
+//import com.bugsnag.BugsnagReactNative;
 import com.microsoft.appcenter.reactnative.crashes.AppCenterReactNativeCrashesPackage;
 import com.microsoft.appcenter.reactnative.analytics.AppCenterReactNativeAnalyticsPackage;
 import com.microsoft.appcenter.reactnative.appcenter.AppCenterReactNativePackage;
@@ -46,7 +46,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
             new MainReactPackage(),
-            BugsnagReactNative.getPackage(),
+//            BugsnagReactNative.getPackage(),
             new AppCenterReactNativeCrashesPackage(MainApplication.this, getResources().getString(R.string.appCenterCrashes_whenToSendCrashes)),
             new AppCenterReactNativeAnalyticsPackage(MainApplication.this, getResources().getString(R.string.appCenterAnalytics_whenToEnableAnalytics)),
             new AppCenterReactNativePackage(MainApplication.this),
