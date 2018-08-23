92f7d18c491f78900f6321ddfbc4ccd021a3d659
ivan <ivan@haxibiao.com>
2018-08-24 00:18:39 +0800

    把官方专题数据放入单独AppData对象，方便后面不同的APP热更新UI里的官方专题小图标

---
+++ constants/AppData.js
@@ -0,0 +1,93 @@
+export const official_categories = [
+	{
+		id: 1,
+		avatar: "https://ainicheng.com/images/appicons/wodeguanzhu.png",
+		name: "关注的专题",
+		type: "全部关注",
+		filter: "CATEGORY"
+	},
+	{
+		id: 64,
+		avatar: "https://www.ainicheng.com/images/appicons/guanfangketang.png",
+		name: "官方课堂",
+		type: "专题详情"
+	},
+	{
+		id: 51,
+		avatar: "https://ainicheng.com/images/appicons/jingxuantougao.png",
+		name: "精选投稿",
+		type: "专题详情"
+	},
+	{
+		id: 5,
+		avatar: "https://ainicheng.com/images/appicons/youxizixun.png",
+		name: "游戏资讯",
+		type: "专题详情"
+	},
+	{
+		id: 85,
+		avatar: "https://ainicheng.com/images/appicons/steam.png",
+		name: "steam",
+		type: "专题详情"
+	},
+	{
+		id: 12,
+		avatar: "https://ainicheng.com/images/appicons/yingxionglianmeng.png",
+		name: "英雄联盟",
+		type: "专题详情"
+	},
+	{
+		id: 87,
+		avatar: "https://ainicheng.com/images/appicons/juedidataosha.png",
+		name: "绝地求生",
+		type: "专题详情"
+	},
+	{
+		id: 13,
+		avatar: "https://ainicheng.com/images/appicons/wangzerongyao.png",
+		name: "王者荣耀",
+		type: "专题详情"
+	},
+	{
+		id: 6,
+		avatar: "https://ainicheng.com/images/appicons/dota2.png",
+		name: "dota2",
+		type: "专题详情"
+	},
+	{
+		id: 86,
+		avatar: "https://ainicheng.com/images/appicons/duanyou.png",
+		name: "端游",
+		type: "专题详情"
+	},
+	{
+		id: 84,
+		avatar: "https://ainicheng.com/images/appicons/shouyou.png",
+		name: "手游",
+		type: "专题详情"
+	},
+	{
+		id: 46,
+		avatar: "https://ainicheng.com/images/appicons/biaoqingbao.png",
+		name: "表情包",
+		type: "专题详情"
+	},
+	{
+		id: 47,
+		avatar: "https://ainicheng.com/images/appicons/touxiang.png",
+		name: "头像",
+		type: "专题详情"
+	},
+	{
+		id: 1,
+		avatar: "https://ainicheng.com/images/appicons/nicheng.png",
+		name: "昵称",
+		type: "专题详情"
+	},
+	{
+		id: 72,
+		avatar: "https://ainicheng.com/images/appicons/xinqing.png",
+		name: "心情",
+		type: "专题详情"
+	}
+];
--- screens/find/OfficialCategories.js
+++ screens/find/OfficialCategories.js
@@ -4,100 +4,7 @@ import { StyleSheet, View, TouchableOpacity, Text } from "react-native";
 import OfficialColumn from "../../components/Category/OfficialColumn";
 
 import Colors from "../../constants/Colors";
-
-const official_categories = [
-	{
-		id: 1,
-		avatar: "https://ainicheng.com/images/appicons/wodeguanzhu.png",
-		name: "关注的专题",
-		type: "全部关注",
-		filter: "CATEGORY"
-	},
-	{
-		id: 64,
-		avatar: "https://www.ainicheng.com/images/appicons/guanfangketang.png",
-		name: "官方课堂",
-		type: "专题详情"
-	},
-	{
-		id: 51,
-		avatar: "https://ainicheng.com/images/appicons/jingxuantougao.png",
-		name: "精选投稿",
-		type: "专题详情"
-	},
-	{
-		id: 5,
-		avatar: "https://ainicheng.com/images/appicons/youxizixun.png",
-		name: "游戏资讯",
-		type: "专题详情"
-	},
-	{
-		id: 85,
-		avatar: "https://ainicheng.com/images/appicons/steam.png",
-		name: "steam",
-		type: "专题详情"
-	},
-	{
-		id: 12,
-		avatar: "https://ainicheng.com/images/appicons/yingxionglianmeng.png",
-		name: "英雄联盟",
-		type: "专题详情"
-	},
-	{
-		id: 87,
-		avatar: "https://ainicheng.com/images/appicons/juedidataosha.png",
-		name: "绝地求生",
-		type: "专题详情"
-	},
-	{
-		id: 13,
-		avatar: "https://ainicheng.com/images/appicons/wangzerongyao.png",
-		name: "王者荣耀",
-		type: "专题详情"
-	},
-	{
-		id: 6,
-		avatar: "https://ainicheng.com/images/appicons/dota2.png",
-		name: "dota2",
-		type: "专题详情"
-	},
-	{
-		id: 86,
-		avatar: "https://ainicheng.com/images/appicons/duanyou.png",
-		name: "端游",
-		type: "专题详情"
-	},
-	{
-		id: 84,
-		avatar: "https://ainicheng.com/images/appicons/shouyou.png",
-		name: "手游",
-		type: "专题详情"
-	},
-	{
-		id: 46,
-		avatar: "https://ainicheng.com/images/appicons/biaoqingbao.png",
-		name: "表情包",
-		type: "专题详情"
-	},
-	{
-		id: 47,
-		avatar: "https://ainicheng.com/images/appicons/touxiang.png",
-		name: "头像",
-		type: "专题详情"
-	},
-	{
-		id: 1,
-		avatar: "https://ainicheng.com/images/appicons/nicheng.png",
-		name: "昵称",
-		type: "专题详情"
-	},
-	{
-		id: 72,
-		avatar: "https://ainicheng.com/images/appicons/xinqing.png",
-		name: "心情",
-		type: "专题详情"
-	}
-];
+import { official_categories } from "../../constants/AppData";
 
 class OfficialCategories extends React.Component {
 	render() {
@@ -140,7 +47,14 @@ class OfficialCategories extends React.Component {
 			<TouchableOpacity
 				key={index}
 				style={{ flex: 1 }}
-				onPress={() => navigate(item.type, item.type == "专题详情" ? { category: item } : { filter: item.filter })}
+				onPress={() =>
+					navigate(
+						item.type,
+						item.type == "专题详情"
+							? { category: item }
+							: { filter: item.filter }
+					)
+				}
 			>
 				<OfficialColumn data={item} />
 			</TouchableOpacity>
