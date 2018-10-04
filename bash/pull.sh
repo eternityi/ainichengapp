e3204b68f3e05c6d7e48c37c9888337c907b5f78
ivan zhang <ivan@ivans-MacBook-Pro.local>
2018-10-04 21:55:39 +0800

    ./bash/diff.sh 没有一个个亲自用git gui工具检查的效果好

--- bash/pull.sh
+++ bash/pull.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 
+echo "拉取 ... ainicheng"
 cd /data/app/ainicheng
 git stash -u
 git pull
 
+echo "拉取 ... dongmeiwei"
 cd /data/app/dongmeiwei
 git stash -u
 git pull
 
+echo "拉取 ... dianmoge"
 cd /data/app/dianmoge
 git stash -u
 git pull
 
+echo "拉取 ... jucheshe"
 cd /data/app/jucheshe
 git stash -u
 git pull
 
+echo "拉取 ... youwangfa"
 cd /data/app/youwangfa
 git stash -u
 git pull
 
+echo "拉取 ... jinlinle"
 cd /data/app/jinlinle
 git stash -u
 git pull
